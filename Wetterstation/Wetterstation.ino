7f41b01dea20a387ead0379a14b02967ffb0bcea
Marvin Schmidt <marv@exherbo.org>
2014-10-01 22:17:54 +0100

    Add arduino program to read anemometer measurements

---
+++ Wetterstation/Wetterstation.ino
@@ -0,0 +1,28 @@
+#include <Anemometer.h>
+
+Anemometer anemo(2, 3);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Starting up...");
+}
+
+void loop() {
+  Serial.println("Reading anemometer measurement");
+  
+  anemometer_reading ar;
+  ar = anemo.getMeasurement();
+
+  Serial.print("CRC: ");
+  Serial.println(anemo.isValid(ar));
+  Serial.println();
+  
+  Serial.print("Wind direction: ");
+  Serial.println(anemo.windDirectionToString(ar.fields.wind_direction));
+  
+  Serial.print("Wind speed: ");
+  Serial.println(ar.fields.wind_speed));
+  
+  delay(10);
+}
+
