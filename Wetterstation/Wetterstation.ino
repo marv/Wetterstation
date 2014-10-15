6e2aa2df3011f4f10a677d746588e6f27d8466ef
Marvin Schmidt <marv@exherbo.org>
2014-10-15 12:08:43 +0100

    Style

--- Wetterstation/Wetterstation.ino
+++ Wetterstation/Wetterstation.ino
@@ -1,28 +1,30 @@
+/* vim: set sw=4 sts=4 et ft=cpp foldmethod=syntax : */
+
 #include "Anemometer.h"
 
 Anemometer anemo(2, 3);
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Starting up...");
+    Serial.begin(9600);
+    Serial.println("Starting up...");
 }
 
 void loop() {
-  Serial.println("Reading anemometer measurement");
-  
-  anemometer_reading ar;
-  ar = anemo.getMeasurement();
+    Serial.println("Reading anemometer measurement");
 
-  Serial.print("CRC: ");
-  Serial.println(anemo.isValid(ar));
-  Serial.println();
-  
-  Serial.print("Wind direction: ");
-  Serial.println(anemo.windDirectionToString(ar.fields.wind_direction));
-  
-  Serial.print("Wind speed: ");
-  Serial.println(ar.fields.wind_speed);
-  
-  delay(10);
+    anemometer_reading ar;
+    ar = anemo.getMeasurement();
+
+    Serial.print("CRC: ");
+    Serial.println(anemo.isValid(ar));
+    Serial.println();
+
+    Serial.print("Wind direction: ");
+    Serial.println(anemo.windDirectionToString(ar.fields.wind_direction));
+
+    Serial.print("Wind speed: ");
+    Serial.println(ar.fields.wind_speed);
+
+    delay(10);
 }
 
