a9ccea6170f7e5607bef723b2bf7a966a1bdc1dc
Marvin Schmidt <marv@exherbo.org>
2014-10-15 14:13:12 +0100

    Add BMP085 sensor

--- Wetterstation/Wetterstation.ino
+++ Wetterstation/Wetterstation.ino
@@ -3,6 +3,7 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <RTClib.h>
+#include <Adafruit_BMP085.h>
 
 #include "Pinning.h"
 #include "Anemometer.h"
@@ -10,6 +11,7 @@
 Anemometer anemo(PIN_ANEMOMETER_DATA, PIN_ANEMOMETER_ENABLE);
 
 RTC_DS1307 rtc;
+Adafruit_BMP085 bmp;
 
 void setup() {
     Serial.begin(9600);
@@ -42,6 +44,44 @@ void setup() {
     sprintf(timeString, "%02d:%02d:%02d", now.hour(), now.minute(), now.second());
     Serial.print("DS1307 RTC time: ");
     Serial.println(timeString);
+
+
+    /** BMP085 **/
+    if (!bmp.begin())
+    {
+        Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+        while (1) {}
+    }
+}
+
+void
+gather_bmp085_data()
+{
+    Serial.print("  Temperature = ");
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print("  Pressure = ");
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    // Calculate altitude assuming 'standard' barometric
+    // pressure of 1013.25 millibar = 101325 Pascal
+    Serial.print("  Altitude = ");
+    Serial.print(bmp.readAltitude());
+    Serial.println(" meters");
+
+    Serial.print("  Pressure at sealevel (calculated) = ");
+    Serial.print(bmp.readSealevelPressure());
+    Serial.println(" Pa");
+
+    // you can get a more precise measurement of altitude
+    // if you know the current sea level pressure which will
+    // vary with weather and such. If it is 1015 millibars
+    // that is equal to 101500 Pascals.
+    Serial.print("  Real altitude = ");
+    Serial.print(bmp.readAltitude(101500));
+    Serial.println(" meters");
 }
 
 void
@@ -69,6 +109,9 @@ void loop() {
 
     Serial.println(" * Anemometer:");
     gather_anemometer_data();
+
+    Serial.println(" * BMP085:");
+    gather_bmp085_data();
 
     delay(10);
 }
